2e04a10ddb70b48b8c6ea6b8bd69e0cc45a7ffde
pazzo83 <uvapazzo@gmail.com>
2017-05-10 09:51:38 -0400

    first commit

---
+++ src/html_strings.jl
@@ -0,0 +1,322 @@
+macro R_str(s)
+    s
+end
+
+const RENTALS_HTML = R"""<div bgcolor=3D"#EDE7D7" text=3D"#333333" link=3D"#3F5374" vlink=3D"#3F5374=
+" alink=3D"#3F5374" style=3D"padding:0;margin:0;font:Arial,Helvetica,sans-s=
+erif;font-family:Arial,Helvetica,sans-serif">
+=09<div style=3D"background-color:#ede7d7;padding:2px 10px 10px 10px">
+=09
+=09<table width=3D"634" border=3D"0" cellspacing=3D"0" cellpadding=3D"0" al=
+ign=3D"center">
+=09=09<tbody><tr>
+=09=09=09<td style=3D"padding-bottom:8px;font-family:Arial,Helvetica,sans-s=
+erif;font-size:10px;color:#3f5374">
+=09=09=09=09If you would prefer to view this email in your web browser, <a =
+href=3D"http://click.message.rentals.com/?qs=3Dc11e2871c9ace279fd440b4658cb=
+c770f3788d6520e1767217f77a1a717f5c821c942f190530e6a7e9d381b072cac0e1edb9d2e=
+59ef369f0" style=3D"color:#3f5374" target=3D"_blank">click here.</a>
+=09=09=09</td>
+=09=09</tr>
+=09</tbody></table>
+=09
+=09<table width=3D"634" border=3D"0" cellspacing=3D"0" cellpadding=3D"0" al=
+ign=3D"center">
+=09=09<tbody><tr valign=3D"bottom" style=3D"background:#f7f4ed">
+=09=09=09<td width=3D"154"><img src=3D"http://image.message.rentals.com/lib=
+/fe9a13707565017c72/m/1/hdr_tlcorner.gif" height=3D"23" width=3D"154" alt=
+=3D"" border=3D"0" style=3D"display:block"></td>
+=09=09=09<td width=3D"467" align=3D"right" style=3D"font-family:Arial,Helve=
+tica,sans-serif;font-size:10px;font-weight:bold;color:#3f5273"><a href=3D"h=
+ttp://click.message.rentals.com/?qs=3Debbf5c49b3a6da9c02f64e23827b1311474e6=
+cf40d05183f3af6e366271fccfd882e4149cc5b4c1e3beabe4c2c4b2d0cf37ad14635332b1e=
+d3bf33e1941eaa42" target=3D"_blank">SIGN INTO YOUR ACCOUNT</a>=C2=A0=C2=A0|=
+=C2=A0=C2=A0<a href=3D"http://click.message.rentals.com/?qs=3D2367563e7c4f8=
+40040b2eaad2c0382604c3c69249249e72e7418dff72e0548925dda7c7bc3e6349268c5e8ad=
+1d32111cc3cd0ff0357ac2ed311917d310aa2e5c" target=3D"_blank">RENTALS.COM</a>=
+</td>
+=09=09=09<td width=3D"13"><img src=3D"http://image.message.rentals.com/lib/=
+fe9a13707565017c72/m/1/hdr_trcorner.gif" height=3D"23" width=3D"13" alt=3D"=
+--" border=3D"0"></td>
+=09=09</tr>
+=09</tbody></table>
+=09
+=09<table width=3D"634" border=3D"0" cellspacing=3D"0" cellpadding=3D"0" al=
+ign=3D"center">
+=09=09<tbody><tr valign=3D"top">
+=09=09=09<td width=3D"329">
+=09=09=09=09<a href=3D"http://click.message.rentals.com/?qs=3D19f0dc25837df=
+818956b3d25f861fd9ea7c26507490231a91457145a54fff8c5b143f4f86c20942e356be76e=
+e0b43dddb03d4f2840abfc34ff72bdb67af9da57" target=3D"_blank">
+=09=09=09=09=09<img src=3D"http://image.message.rentals.com/lib/fe9a1370756=
+5017c72/m/1/hdr_logo_b.jpg" alt=3D"Rentalhouses.com" border=3D"0" style=3D"=
+display:block">
+=09=09=09=09</a>
+=09=09=09</td>
+=09=09=09<td width=3D"305">
+=09=09=09=09<table width=3D"305" border=3D"0" cellspacing=3D"0" cellpadding=
+=3D"0">
+=09=09=09=09=09<tbody><tr>
+=09=09=09=09=09=09<td height=3D"34">
+=09=09=09=09=09=09=09<img src=3D"http://image.message.rentals.com/lib/fe9a1=
+3707565017c72/m/1/hdr_swoop.gif" height=3D"34" width=3D"305" alt=3D"" borde=
+r=3D"0" style=3D"display:block">
+=09=09=09=09=09=09</td>
+=09=09=09=09=09</tr>
+=09=09=09=09=09<tr valign=3D"top">
+=09=09=09=09=09=09<td width=3D"305" height=3D"55" align=3D"right" style=3D"=
+font-family:Arial,Helvetica,sans-serif;font-size:14px;font-weight:bold;colo=
+r:#3f5273;background:#ffffff;padding-right:13px">=C2=A0</td>
+=09=09=09=09=09</tr>
+=09=09=09=09</tbody></table>
+=09=09=09</td>
+=09=09</tr>
+=09</tbody></table>
+=09
+=09
+=09
+=09<table width=3D"634" border=3D"0" cellspacing=3D"0" cellpadding=3D"0" al=
+ign=3D"center" style=3D"background:#fff">
+=09=09<tbody><tr>
+=09=09=09<td width=3D"12"><img src=3D"http://image.message.rentals.com/lib/=
+fe9a13707565017c72/m/1/trans.gif" height=3D"10" width=3D"12" border=3D"0"><=
+/td>
+=09=09=09<td width=3D"610"><img src=3D"http://image.message.rentals.com/lib=
+/fe9a13707565017c72/m/1/hdr_content.gif" height=3D"13" width=3D"610" alt=3D=
+"" border=3D"0"></td>
+=09=09=09<td width=3D"12"><img src=3D"http://image.message.rentals.com/lib/=
+fe9a13707565017c72/m/1/trans.gif" height=3D"10" width=3D"12" border=3D"0"><=
+/td>
+=09=09</tr>
+=09</tbody></table>
+=09
+=09<table width=3D"634" border=3D"0" cellspacing=3D"0" cellpadding=3D"0" al=
+ign=3D"center" style=3D"background:#fff">
+=09=09<tbody><tr>
+=09=09=09<td width=3D"12"><img src=3D"http://image.message.rentals.com/lib/=
+fe9a13707565017c72/m/1/trans.gif" height=3D"10" width=3D"12" border=3D"0"><=
+/td>
+=09=09=09<td width=3D"608" style=3D"border-left:solid 1px #cbc7ba;border-ri=
+ght:solid 1px #cbc7ba;border-bottom:solid 1px #cbc7ba">
+=09=09=09=09<table width=3D"608" border=3D"0" cellspacing=3D"0" cellpadding=
+=3D"0">
+=09=09=09=09=09<tbody><tr>
+=09=09=09=09=09=09<td align=3D"left" style=3D"font-family:Arial,Helvetica,s=
+ans-serif;font-size:24px;font-weight:bold;color:#4a5846;padding:0 0 8px 10p=
+x;border-bottom:solid 1px #cbc7ba">You&#39;ve Received a Lead on Your Renta=
+l Property Listing</td>
+=09=09=09=09=09</tr>
+=09=09=09=09=09<tr>
+=09=09=09=09=09=09<td align=3D"center" style=3D"border-top:solid 1px #cbc7b=
+a"><img src=3D"http://image.message.rentals.com/lib/fe9a13707565017c72/m/1/=
+top_gradient.gif" height=3D"29" width=3D"608" alt=3D"" border=3D"0" style=
+=3D"display:block"></td>
+=09=09=09=09=09</tr>
+=09=09=09=09=09<tr>
+=09=09=09=09=09=09<td align=3D"left" style=3D"font-family:Arial,Helvetica,s=
+ans-serif;font-size:14px;font-weight:bold;color:#4a5846;padding:0 0 8px 10p=
+x">
+=09=09=09=09=09=09=09A renter has inquired about your rental property.<br>P=
+lease see the Rentals.com lead submission below:
+=09=09=09=09=09=09</td>
+=09=09=09=09=09</tr>
+=09=09=09=09=09<tr>
+=09=09=09=09=09=09<td align=3D"center" style=3D"padding-bottom:20px">
+=09=09=09=09=09=09=09<table width=3D"576" border=3D"0" cellspacing=3D"0" ce=
+llpadding=3D"0">
+=09=09=09=09=09=09=09=09<tbody><tr>
+=09=09=09=09=09=09=09=09=09<td style=3D"font-family:Arial,Helvetica,sans-se=
+rif;font-size:14px;font-weight:bold" width=3D"192" align=3D"left">Ad #<a hr=
+ef=3D"http://click.message.rentals.com/?qs=3D3b28df20d8a286c41c6df42bdfcbf4=
+a9d261114aab3c90d517dc5fc7bec55a7104a5afba003b526ea64a4eb633cc2bc0f4cdd4a1c=
+a8b8c46" target=3D"_blank">2273233</a></td>
+=09=09=09=09=09=09=09=09=09<td style=3D"font-family:Arial,Helvetica,sans-se=
+rif;font-size:14px;font-weight:bold" width=3D"192" align=3D"left">Contact</=
+td>
+=09=09=09=09=09=09=09=09=09<td style=3D"font-family:Arial,Helvetica,sans-se=
+rif;font-size:14px;font-weight:bold" width=3D"192" align=3D"left">Anticipat=
+ed Move Date:</td>
+=09=09=09=09=09=09=09=09</tr>
+=09=09=09=09=09=09=09=09<tr valign=3D"top">
+=09=09=09=09=09=09=09=09=09<td align=3D"left" style=3D"font-family:Arial,He=
+lvetica,sans-serif;font-size:12px;padding:4px 8px 0 0">
+=09=09=09=09=09=09=09=09=09=09247 N Capitol Ave, Unit 176<br>
+=09=09=09=09=09=09=09=09=09=09
+=09=09=09=09=09=09=09=09=09=09San Jose,=20
+=09=09=09=09=09=09=09=09=09=09
+=09=09=09=09=09=09=09=09=09=09CA=20
+=09=09=09=09=09=09=09=09=09=09
+=09=09=09=09=09=09=09=09=09=0995127
+=09=09=09=09=09=09=09=09=09=09
+=09=09=09=09=09=09=09=09=09</td>
+=09=09=09=09=09=09=09=09=09<td align=3D"left" style=3D"font-family:Arial,He=
+lvetica,sans-serif;font-size:12px;padding:4px 8px 0 0">
+=09=09=09=09=09=09=09=09=09=09<p style=3D"margin-top:0">
+=09=09=09=09=09=09=09=09=09=09=09Phoebe=20
+=09=09=09=09=09=09=09=09=09=09=09
+=09=09=09=09=09=09=09=09=09=09=09keene<br>
+=09=09=09=09=09=09=09=09=09=09=09
+=09=09=09=09=09=09=09=09=09=09=09<a href=3D"tel:(818)%20648-2503" value=3D"=
++18186482503" target=3D"_blank">8186482503</a><br>
+=09=09=09=09=09=09=09=09=09=09=09
+=09=09=09=09=09=09=09=09=09=09=09<a href=3D"mailto:keene.phoebe@gmail.com" =
+style=3D"color:#3f5273" target=3D"_blank">keene.phoebe@gmail.com</a>
+=09=09=09=09=09=09=09=09=09=09=09
+=09=09=09=09=09=09=09=09=09=09</p>
+=09=09=09=09=09=09=09=09=09</td>
+=09=09=09=09=09=09=09=09=09<td align=3D"left" style=3D"font-family:Arial,He=
+lvetica,sans-serif;font-size:12px;padding:4px 8px 0 0">
+=09=09=09=09=09=09=09=09=09=09
+=09=09=09=09=09=09=09=09=09=09
+=09=09=09=09=09=09=09=09=09</td>
+=09=09=09=09=09=09=09=09</tr>
+=09=09=09=09=09=09=09</tbody></table>
+=09=09=09=09=09=09</td>
+=09=09=09=09=09</tr>
+=09=09=09=09=09<tr>
+=09=09=09=09=09=09<td align=3D"center" style=3D"border-bottom:solid 1px #cb=
+c7ba;padding-bottom:20px">
+=09=09=09=09=09=09=09<table width=3D"576" border=3D"0" cellspacing=3D"0" ce=
+llpadding=3D"0">
+=09=09=09=09=09=09=09=09<tbody><tr>
+=09=09=09=09=09=09=09=09=09<td align=3D"left" width=3D"192" style=3D"font-f=
+amily:Arial,Helvetica,sans-serif;font-size:14px;font-weight:bold">Additiona=
+l Comments:</td>
+=09=09=09=09=09=09=09=09</tr>
+=09=09=09=09=09=09=09=09<tr valign=3D"top">
+=09=09=09=09=09=09=09=09=09<td align=3D"left" style=3D"font-family:Arial,He=
+lvetica,sans-serif;font-size:12px;padding:4px 8px 0 0">Hi, I found your lis=
+ting on Rent.com and would like to schedule a visit. Thanks!</td>
+=09=09=09=09=09=09=09=09</tr>
+=09=09=09=09=09=09=09</tbody></table>
+=09=09=09=09=09=09</td>
+=09=09=09=09=09</tr>
+=09=09=09=09=09<tr>
+=09=09=09=09=09=09<td style=3D"padding:10px 20px 10px 20px">
+=09=09=09=09=09=09=09<table width=3D"568" border=3D"0" cellspacing=3D"0" ce=
+llpadding=3D"0" style=3D"padding-bottom:10px;padding-top:10px">
+=09=09=09=09=09=09=09=09<tbody><tr>
+=09=09=09=09=09=09=09=09=09<td align=3D"left" style=3D"font-family:Arial,He=
+lvetica,sans-serif;font-size:12px;font-weight:bold">Attention Customers</td=
+>
+=09=09=09=09=09=09=09=09</tr>
+=09=09=09=09=09=09=09=09<tr valign=3D"top">
+=09=09=09=09=09=09=09=09=09<td align=3D"left" style=3D"font-family:Arial,He=
+lvetica,sans-serif;font-size:11px;padding:0 8px 0 0">
+=09=09=09=09=09=09=09=09=09=09<p style=3D"margin-top:4px">Please be aware o=
+f an internet scam that some of our customers have encountered. A potential=
+ tenant will overpay you a large sum of money in the form of a cashier&#39;=
+s check, and ask for the difference back. Do not send any money. The check =
+is fake.</p>
+=09=09=09=09=09=09=09=09=09=09<p style=3D"margin-bottom:0">If you have any =
+questions about this or other questionable activities, please call our cust=
+omer service center at <a href=3D"tel:(888)%20501-7368" value=3D"+188850173=
+68" target=3D"_blank">1-888-501-7368</a>, or, visit The Federal Trade Commi=
+ssion&#39;s website at: <a href=3D"http://click.message.rentals.com/?qs=3D0=
+5b15be59f01f1c9063a65e14829515fb36e1b50c39c48a772eaf2c9b98e562876ffb516470e=
+91fd73689c0230d36d7c5e11e8b2269efa70" style=3D"color:#3f5273" target=3D"_bl=
+ank">http://www.ftc.gov</a></p>
+=09=09=09=09=09=09=09=09=09</td>
+=09=09=09=09=09=09=09=09</tr>
+=09=09=09=09=09=09=09</tbody></table>
+=09=09=09=09=09=09</td>
+=09=09=09=09=09</tr>
+=09=09=09=09=09<tr>
+=09=09=09=09=09=09<td>
+=09=09=09=09=09=09=09<table align=3D"center" width=3D"608" border=3D"0" cel=
+lspacing=3D"0" cellpadding=3D"0" style=3D"background:#e0ddd6;border:solid 1=
+px #fff">
+=09=09=09=09=09=09=09=09<tbody><tr>
+=09=09=09=09=09=09=09=09=09<td align=3D"left" style=3D"font-family:Arial,He=
+lvetica,sans-serif;font-size:12px;color:#4a5846;padding:10px 10px 10px 10px=
+">
+=09=09=09=09=09=09=09=09=09=09<p>Replies to this message will be sent to Ph=
+oebe keene, at <a href=3D"mailto:keene.phoebe@gmail.com" style=3D"color:#3f=
+5273" target=3D"_blank">keene.phoebe@gmail.com</a> and not Rentals.com.</p>
+=09=09=09=09=09=09=09=09=09=09<p>Thanks for using Rentals.com!</p>
+=09=09=09=09=09=09=09=09=09</td>
+=09=09=09=09=09=09=09=09</tr>
+=09=09=09=09=09=09=09</tbody></table>
+=09=09=09=09=09=09</td>
+=09=09=09=09=09</tr>
+=09=09=09=09=09<tr>
+=09=09=09=09=09=09<td>
+=09=09=09=09=09=09=09<table cellpadding=3D"0" cellspacing=3D"0" border=3D"0=
+" width=3D"100%">
+=09=09=09=09=09=09=09=09<tbody><tr>
+=09=09=09=09=09=09=09=09=09<td style=3D"border-top:solid 1px #cbc7ba" align=
+=3D"center" valign=3D"top">
+=09=09=09=09=09=09=09=09=09=09<a href=3D"http://click.message.rentals.com/?=
+qs=3D4270e354bdff31f3c882bd2af5009caaa95753b2fafb0c38ff395dbce72672b9e53f9f=
+b4e62cb6d8975fcf1fcb62a277ae1db5cb607823d5" target=3D"_blank">
+=09=09=09=09=09=09=09=09=09=09=09<img src=3D"http://image.message.rentals.c=
+om/lib/fe9913707565017c7d/m/1/rentList600x85_C.gif" alt=3D"RentList.com" wi=
+dth=3D"600" height=3D"85" hspace=3D"0" vspace=3D"5" border=3D"0">
+=09=09=09=09=09=09=09=09=09=09</a>
+=09=09=09=09=09=09=09=09=09</td>
+=09=09=09=09=09=09=09=09</tr>
+=09=09=09=09=09=09=09</tbody></table>
+=09=09=09=09=09=09=09
+=09=09=09=09=09=09</td>
+=09=09=09=09=09</tr>
+=09=09=09=09</tbody></table>
+=09=09=09</td>
+=09=09=09<td width=3D"12">
+=09=09=09=09<img src=3D"http://image.message.rentals.com/lib/fe9a1370756501=
+7c72/m/1/trans.gif" height=3D"10" width=3D"12" border=3D"0">
+=09=09=09</td>
+=09=09</tr>
+=09</tbody></table>
+=09
+=09<table width=3D"634" border=3D"0" cellspacing=3D"0" cellpadding=3D"0" al=
+ign=3D"center">
+=09=09<tbody><tr>
+=09=09=09<td><img src=3D"http://image.message.rentals.com/lib/fe9a137075650=
+17c72/m/1/footer.gif" height=3D"17" width=3D"634" alt=3D"" border=3D"0" sty=
+le=3D"display:block"></td>
+=09=09</tr>
+=09</tbody></table>
+=09
+=09
+=09<table width=3D"634" border=3D"0" cellpadding=3D"10" cellspacing=3D"0" a=
+lign=3D"center">
+=09=09<tbody><tr>
+=09=09=09<td>
+=09=09=09=09<table align=3D"center" width=3D"608" border=3D"0" cellspacing=
+=3D"0" cellpadding=3D"0">
+=09=09=09=09=09<tbody><tr>
+=09=09=09=09=09=09<td style=3D"font-family:Arial,Helvetica,sans-serif;font-=
+size:10px;color:#333333" align=3D"center">
+=09=09=09=09=09=09=09<p style=3D"padding:0;margin:0">
+=09=09=09=09=09=09=09=09=C2=A9 2013-2017 RentPath, Inc.  All rights reserve=
+d.  All photos, videos, text and other content are the property of RentPath=
+, Inc.  RENTALS and the RENTALS Trade Dress are registered trademarks of Re=
+ntPath, Inc. or its affiliates.<br>3585 Engineering Drive, Suite 100, Norcr=
+oss, Georgia 30092 | <a href=3D"tel:(800)%20216-1423" value=3D"+18002161423=
+" target=3D"_blank">800-216-1423</a>
+=09=09=09=09=09=09=09</p>
+=09=09=09=09=09=09</td>
+=09=09=09=09=09</tr>
+=09=09=09=09</tbody></table>
+=09=09=09=09<table cellspacing=3D"0" cellpadding=3D"0" width=3D"100%" borde=
+r=3D"0">
+=09=09=09=09=09<tbody><tr>
+=09=09=09=09=09=09<td align=3D"right">
+=09=09=09=09=09=09=09<font style=3D"FONT-SIZE:11px;COLOR:#333333"><br>
+=09=09=09=09=09=09=09=09
+=09=09=09=09=09=09=09=09<a href=3D"http://click.message.rentals.com/?qs=3Dc=
+44b4b43269b04c7739766cdc876b17a8c58591b8756f41f471de272203b03a148eb9f11f3e8=
+4c3dff56f2441d30f6e224b396e065335c4100deaff1b2e12c3a" target=3D"_blank">Cli=
+ck here</a> to visit Rentals.com&#39;s Privacy Policy.
+=09=09=09=09=09=09=09</font>
+=09=09=09=09=09=09</td>
+=09=09=09=09=09</tr>
+=09=09=09=09</tbody></table>
+=09=09=09</td>
+=09=09</tr>
+=09</tbody></table>
+</div>
+<img src=3D"http://click.message.rentals.com/open.aspx?ffcb10-fec41371746c0=
+574-fe261c747264077a761c71-fe9a13707565017c72-ff61107776-fe2815747d61077d70=
+1d74-fec9167271640174" width=3D"1" height=3D"1">
+</div>"""
